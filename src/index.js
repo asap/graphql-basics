2743c84800359cec8c570539eeda637e3b88f6df
ASAP <alexander.sapountzis@gmail.com>
2019-01-16 21:49:27 -0500

    Add deleteComment mutation

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ const typeDefs = `
     createPost(data: CreatePostInput!): Post!
     deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -219,6 +220,17 @@ const resolvers = {
 
       comments.push(comment);
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(comment => comment.id === args.id);
+
+      if (commentIndex === -1) {
+        throw new Error('Comment does not exist');
+      }
+
+      const deletedComment = comments.splice(commentIndex, 1);
+
+      return deletedComment[0];
     },
   },
   Comment: {
